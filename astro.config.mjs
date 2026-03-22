56f5bfe1ec2e96c694e7fdceed0deb36b1716cef
Kingsley Leung <kingsleyleung2003@outlook.com>
2026-03-22 22:59:43 +1300

    Revert "feat: Introduce a comprehensive set of UI components, along with new utilities, hooks, and updated project configurations."
    This reverts commit 41a1f7f1700df880622117e2ca46b845b85b222c.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,9 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
-
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), react()],
-    vite: {
-        plugins: [tailwindcss()],
-    },
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
