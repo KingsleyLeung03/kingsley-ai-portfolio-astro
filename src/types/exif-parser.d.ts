a44c9c1ca024e74f9f558eeed7b899320e9740f1
Kingsley Leung <kingsleyleung2003@outlook.com>
2026-04-04 14:28:28 +1300

    chore: integrate Biome for linting and formatting and apply codebase-wide style updates

--- src/types/exif-parser.d.ts
+++ src/types/exif-parser.d.ts
@@ -1,11 +1,11 @@
 declare module "exif-parser" {
-  export function create(buffer: Buffer): {
-    parse(): {
-      tags: {
-        DateTimeOriginal?: number;
-        [key: string]: any;
-      };
-      [key: string]: any;
-    };
-  };
+	export function create(buffer: Buffer): {
+		parse(): {
+			tags: {
+				DateTimeOriginal?: number;
+				[key: string]: unknown;
+			};
+			[key: string]: unknown;
+		};
+	};
 }
