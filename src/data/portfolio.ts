7f64edd6b85dcf006d7d47b3918ff7e040a4bc74
Kingsley Leung <kingsleyleung2003@outlook.com>
2026-04-05 18:14:18 +1200

    chore: update resume to DOCX format and refactor text wrapping utility classes

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -6,7 +6,7 @@ export const portfolioData = {
 		avatar: "/about/avatar.jpg", // We will have to move avatar.jpg to public/about/avatar.jpg
 		summary:
 			"A high-achieving Computer Science graduate (GPA 7.1/9) who led a six-member team to develop an AI-powered web platform for wildlife conservation. Skilled in full-stack development, recognised for being collaborative and continuously learning new tech stacks like AWS as needed, demonstrated through an internship at Endace and multiple projects. Proficient in DevOps practices, including Git, Docker, CI/CD. Ready to leverage a strong foundation in C#, TypeScript and Python.",
-		resumeUrl: "/resume/Zihong_Liang_CV.pdf",
+		resumeUrl: "/resume/Zihong_Liang_CV.docx",
 		transcriptUrl: "/transcript/Zihong_Liang_Auckland_Transcript.pdf",
 	},
 	contact: {
